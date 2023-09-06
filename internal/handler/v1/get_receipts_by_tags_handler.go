4b205c947e39f9e489fa5fce65c7bace34ac4261
Mario Georgiev <30326464+despondency@users.noreply.github.com>
2023-09-06 14:23:31 +0300

    Fix endpoint use query params for the tags, comma separated

--- internal/handler/v1/get_receipts_by_tags_handler.go
+++ internal/handler/v1/get_receipts_by_tags_handler.go
@@ -6,11 +6,8 @@ import (
 	"fmt"
 	"github.com/despondency/toggl-task/internal/service"
 	"github.com/gofiber/fiber/v2"
+	"strings"
 )
-
-type GetReceiptsByTagsRequest struct {
-	Tags []string `json:"tags"`
-}
 
 type GetReceiptsByTagsResultHandler struct {
 	receiptService service.ReceiptServicer
@@ -22,14 +19,19 @@ func NewGetReceiptsByTagResultHandler(uploadSvc service.ReceiptServicer) *GetRec
 
 func (grrh *GetReceiptResultHandler) GetReceiptsByTagHandler() func(c *fiber.Ctx) error {
 	handler := func(c *fiber.Ctx) error {
-		body := c.Body()
-		req := &GetReceiptsByTagsRequest{}
-		err := json.Unmarshal(body, req)
-		if err != nil {
-			c.Response().AppendBodyString(fmt.Sprintf("err: %v", err))
-			return c.SendStatus(fiber.StatusInternalServerError)
+		queries := c.Query("tags")
+		queries = strings.TrimSpace(queries)
+		if len(queries) == 0 {
+			return c.SendStatus(fiber.StatusBadRequest)
 		}
-		r, err := grrh.receiptService.GetReceiptsByTags(context.Background(), req.Tags)
+		tagsSeparated := strings.Split(queries, ",")
+		if len(tagsSeparated) == 0 {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		if checkIfTagsEmpty(tagsSeparated) {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		r, err := grrh.receiptService.GetReceiptsByTags(context.Background(), tagsSeparated)
 		if err != nil {
 			c.Response().AppendBodyString(fmt.Sprintf("err: %v", err))
 			return c.SendStatus(fiber.StatusInternalServerError)
@@ -41,4 +43,13 @@ func (grrh *GetReceiptResultHandler) GetReceiptsByTagHandler() func(c *fiber.Ctx
 		return c.SendString(string(jsonStr))
 	}
 	return handler
+}
+
+func checkIfTagsEmpty(separated []string) bool {
+	for _, tag := range separated {
+		if len(strings.TrimSpace(tag)) == 0 {
+			return true
+		}
+	}
+	return false
 }
